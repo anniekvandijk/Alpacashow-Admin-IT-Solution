2a255b1521e3eb55e6bd2b02157ee3bdf74f88ec
Anniek van Dijk <anniek@animundo.nl>
2016-12-02 17:35:44 +0100

    Configuration settings

---
+++ Alpacashow-Admin-SpecflowTests/Configuration/Hooks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Alpacashow_Admin_SpecflowTests.Configuration
+{
+   /// <summary>
+   ///
+   /// </summary>
+   [Binding]
+   public static class Hooks
+   {
+      /// <summary>
+      /// Befores the feature.
+      /// </summary>
+      [BeforeFeature]
+      public static void BeforeFeature()
+      {
+         var settings = Settings.Instance.ActiveSettings;
+
+         FeatureContext.Current.Add("Settings-url", settings.Url);
+      }
+   }
+}
---
+++ Alpacashow-Admin-SpecflowTests/Configuration/Settings.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Alpacashow_Admin_SpecflowTests.Configuration
+{
+   /// <summary>
+   /// The Settings.
+   /// </summary>
+   [XmlRoot(ConfigurationSectionName)]
+   public class Settings
+   {
+      /// <summary>
+      /// The name of the configuration section.
+      /// </summary>
+      internal const string ConfigurationSectionName = "environmentSettings";
+
+      private static readonly Lazy<Settings> _lazyInstance;
+
+      /// <summary>
+      /// Initializes static members of the <see cref="Settings" /> class.
+      /// </summary>
+      static Settings()
+      {
+         _lazyInstance = new Lazy<Settings>(ReadConfiguration);
+      }
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="Settings" /> class.
+      /// </summary>
+      internal Settings()
+      {
+      }
+
+      /// <summary>
+      /// Gets the singeton instance of the <see cref="Settings"/> class.
+      /// </summary>
+      public static Settings Instance => _lazyInstance.Value;
+
+      /// <summary>
+      /// Gets or sets the active platform.
+      /// </summary>
+      public string ActivePlatform { get; set; }
+
+      /// <summary>
+      /// Gets or sets the active platform.
+      /// </summary>
+      [XmlIgnore]
+      public Platform ActiveSettings => Platforms.Single(p => p.Name == ActivePlatform);
+
+      /// <summary>
+      /// Gets or sets the platforms.
+      /// </summary>
+      [XmlArray("Platforms"), XmlArrayItem("Platform")]
+      public Platform[] Platforms { get; set; }
+
+      private static Settings ReadConfiguration()
+      {
+         var settings = XmlSection<Settings>.GetSettings(ConfigurationSectionName);
+         return settings;
+      }
+   }
+}
+
+}
