5225f3dfffa1124f32a2f8706909996ad34d6b23
Anniek van Dijk <anniek@animundo.nl>
2016-12-02 17:01:07 +0100

    Add project files.

---
+++ Alpacashow-Admin-SpecflowTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alpacashow_Admin_SpecflowTests
+{
+   [TestClass]
+   public class UnitTest1
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+      }
+   }
+}
